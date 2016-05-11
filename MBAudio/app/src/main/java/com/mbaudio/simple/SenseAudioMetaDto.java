2624e95b20d1ae49f7b95e73a9853585cb931360
w446108264 <446108264@qq.com>
2016-05-11 10:55:03 +0800

    support jcenter

--- MBAudio/app/src/main/java/com/mbaudio/simple/SenseAudioMetaDto.java
+++
@@ -1,7 +0,0 @@
-package com.mbaudio.simple;
-
-public class SenseAudioMetaDto extends BaseDto {
-    public double duration;
-    public double record_dt;
-    public double sample_rate;
-}
--- MBAudio/app/src/main/java/com/mbaudio/simple/SenseAudioWaveformDto.java
+++
@@ -1,5 +0,0 @@
-package com.mbaudio.simple;
-
-public class SenseAudioWaveformDto  extends BaseDto {
-    public String samples;
-}
